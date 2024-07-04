9f154958a3e6f1b226e098688fdec33dc28e9428
sabeeh01 <sabeeh.javaid@socialpie.io>
2024-07-04 14:08:08 +0500

    lazy loaded modules error handling

--- ERP-container/src/Router/MicrofrontendSwitch.jsx
+++ ERP-container/src/Router/MicrofrontendSwitch.jsx
@@ -1,25 +1,41 @@
 import { Navigate, useLocation } from "react-router-dom";
 import PrivateRoute from "./PrivateRoute";
-import { Suspense, lazy } from "react";
+import { lazy } from "react";
 
 const routes = {
-  "/admin": lazy(() => import("ERP_admin/Routes")),
-  "/invoice": lazy(() => import("ERP_invoice/Routes")),
-  "/user": lazy(() => import("ERP_user/Routes")),
-  "/finance": lazy(() => import("ERP_finance/Routes")),
+  "/admin": lazy(() =>
+    import("ERP_admin/Routes").catch(() => {
+      return { default: () => <>Component unavailable!</> };
+    })
+  ),
+  "/invoice": lazy(() =>
+    import("ERP_invoice/Routes").catch(() => {
+      return { default: () => <>Component unavailable!</> };
+    })
+  ),
+  "/user": lazy(() =>
+    import("ERP_user/Routes").catch(() => {
+      return { default: () => <>Component unavailable!</> };
+    })
+  ),
+  "/finance": lazy(() =>
+    import("ERP_finance/Routes").catch(() => {
+      return { default: () => <>Component unavailable!</> };
+    })
+  ),
 };
 
 const MicrofrontendSwitch = () => {
   const location = useLocation();
-  const basePath = Object.keys(routes).find((path) => location.pathname.startsWith(path));
-  
+  const basePath = Object.keys(routes).find((path) =>
+    location.pathname.startsWith(path)
+  );
+
   if (basePath) {
     const Component = routes[basePath];
     return (
       <PrivateRoute>
-        <Suspense fallback={<div>Loading...</div>}>
-          <Component />
-        </Suspense>
+        <Component />
       </PrivateRoute>
     );
   }
--- ERP-container/src/Router/PrivateRoute.jsx
+++ ERP-container/src/Router/PrivateRoute.jsx
@@ -1,11 +1,14 @@
 import { Suspense } from "react";
 import { Navigate } from "react-router-dom";
+import ErrorBoundary from "../components/ErrorBoundary";
 
 const PrivateRoute = ({ children }) => {
   const token = localStorage.getItem("token");
 
   return token ? (
-    <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>
+    <ErrorBoundary>
+      <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>
+    </ErrorBoundary>
   ) : (
     <Navigate to="/login" />
   );
